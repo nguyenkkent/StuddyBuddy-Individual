8dede245c60d11bcdd0c6e45221b5b7a84f22028
NhanNguyen <nnguyen14@sfsu.edu>
2024-03-14 15:30:28 -0700

    add Dashboard page

--- application/frontend/src/components/Navbar.js
+++ application/frontend/src/components/Navbar.js
@@ -9,6 +9,7 @@ function Navbar() {
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
       <Link to="/aboutus">About Us</Link>
+      <Link to="/dashboard">Dashboard</Link>
     </nav>
   );
 }
