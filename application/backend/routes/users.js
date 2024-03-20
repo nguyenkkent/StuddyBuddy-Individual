4090e2f3b2ecd3f7a584d8487ee7ea9c39e97368
nguyenkkent <nguyen.k.kent@gmail.com>
2024-03-19 17:20:08 -0700

    Pulling in dashboard

--- application/backend/routes/users.js
+++ application/backend/routes/users.js
@@ -1,14 +1,13 @@
 import express from "express";
 import {Users} from "../models/userSchema.js";
 const router = express.Router();
+const bcrypt = require("bcrypt")
 
 router.post('/', async (request, response)=>{
 
     try{
         const item = Users.findOne({email: request.body.email})
         if (item==null){
-            console.log("User does not exists");
-
             const newUser = {
             username : request.body.username,
             password: request.body.password,
@@ -23,15 +22,15 @@ router.post('/', async (request, response)=>{
             
         }
         else{
-
             console.log("email is in used");
+            return response.status(400).send("Email is already in used")
         }
     }
     catch(error){
         console.log("Error: ", error.message);
         response.status(500).send({message: error.message})
     }
-
+    
 
 
 })
