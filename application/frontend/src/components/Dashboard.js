d2b14bfe2653878c3db554c27be09865decbe2c6
Ashley C <ashleyc@Ashleys-MacBook-Air-2.local>
2024-03-22 22:56:05 -0700

    hardcode backend ip address fix

--- application/frontend/src/components/Dashboard.js
+++ application/frontend/src/components/Dashboard.js
@@ -14,7 +14,7 @@ function Dashboard() {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await axios.get("http://localhost:3000/dashboard");
+                const response = await axios.get("http://192.9.228.148:3000/dashboard");
                 setAllUsers(response.data.userData);
                 setFilteredUsers(response.data.userData);
             } catch (error) {
