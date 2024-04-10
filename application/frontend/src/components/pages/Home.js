95d70392a37d7919fd6f4cd322bf18d4cf2a0940
Ashley Ching <aching4@sfsu.edu>
2024-04-10 14:47:26 -0700

    home page skeleton

--- application/frontend/src/components/pages/Home.js
+++ application/frontend/src/components/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import "../../css/Home.css"
+import { Link } from 'react-router-dom';
 
+// Current is only a skeleton
 function Home() {
     return (
       <div>
-        <div className="home-phrase">
-          Blurb of Words
+        <div className="home-main">
+          <div className="home-phrase">
+            <h1>Welcome</h1>
+          </div>
+          <div className="home-start">
+            <Link to="/register">Get Started</Link>
+          </div>
         </div>
       </div>
     );
