dce48c64a2f7f87f03964c84219e0bcfb9dfde16
Ashley Ching <aching4@sfsu.edu>
2024-04-10 12:51:19 -0700

    create common profile component

---
+++ application/frontend/src/components/profiles/Brenden.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Profile from '../common/profile';
+
+function Brenden() {
+  return (
+    <Profile
+      name="Brenden B Lapuz"
+      role="Scrum Master"
+      bio="Hey everyone, my name is Brenden Lapuz, I'm a fourth year here in SFSU.
+      I enjoy playing basketball with my friends on campus. #intramuralchampion2024"
+      img="/bren.JPG"
+    />
+  );
+}
+
+export default Brenden;
---
+++ application/frontend/src/components/profiles/Yuquan.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Profile from '../common/profile';
+
+function Yuquan() {
+  return (
+    <Profile
+      name="Yuquan Xu"
+      role="Front-End Lead"
+      bio="I have made grave mistakes trying to cook meat balls before.
+      I am also a student."
+      img="/yuquan.png"
+    />
+  );
+}
+
+export default Yuquan;
