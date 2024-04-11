5c93cd79da2ff985ec899b77a16ceaaae50016f1
NhanNguyen <nnguyen14@sfsu.edu>
2024-04-10 22:44:46 -0700

    add MyGroups page

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -17,6 +17,7 @@ import AboutUs from './components/pages/AboutUs';
 import Dashboard from './components/pages/Dashboard';
 
 import MyFriends from "./components/pages/MyFriends";
+import MyGroups from "./components/pages/MyGroups";
 import Chats from "./components/pages/Chats";
 import Settings from "./components/pages/Settings";
 
@@ -38,6 +39,7 @@ function App() {
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/my-friends" element={<MyFriends />} />
+          <Route path="/my-groups" element={<MyGroups />} />
           <Route path="/chats" element={<Chats />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
