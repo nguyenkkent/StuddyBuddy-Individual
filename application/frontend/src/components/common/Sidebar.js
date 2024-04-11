49548225b5fdfbad84f811141eb3407bacf37fbd
Ashley Ching <aching4@sfsu.edu>
2024-04-10 17:33:34 -0700

    actually filter by tags

--- application/frontend/src/components/common/Sidebar.js
+++ application/frontend/src/components/common/Sidebar.js
@@ -5,11 +5,11 @@ import '../../css/Sidebar.css';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink>
-      <NavLink activeClassName="active" to="/my-friends">My Friends</NavLink>
-      <NavLink activeClassName="active" to="/my-groups">My Groups</NavLink>
-      <NavLink activeClassName="active" to="/chats">Chats</NavLink>
-      <NavLink activeClassName="active" to="/settings">Settings</NavLink>
+      <NavLink activeclassname="active" to="/dashboard">Dashboard</NavLink>
+      <NavLink activeclassname="active" to="/my-friends">My Friends</NavLink>
+      <NavLink activeclassname="active" to="/my-groups">My Groups</NavLink>
+      <NavLink activeclassname="active" to="/chats">Chats</NavLink>
+      <NavLink activeclassname="active" to="/settings">Settings</NavLink>
       <div className="logout-divider"></div>
       <button onClick={handleLogout}>Logout</button>
     </div>
