5e6ba11d83656b73f5cf41942f33b3ea8390b6ab
nguyenkkent <nguyen.k.kent@gmail.com>
2024-04-23 12:53:35 -0700

    Completed the my-friend querry

--- application/backend/routes/friends.js
+++ application/backend/routes/friends.js
@@ -1,27 +1,37 @@
 import express from "express";
 import {Users} from "../models/userSchema.js";
 import requireAuth from "../middleware/requireAuth.js"
+import {ObjectId} from "mongodb";
 
 const router = express.Router();
 router.use(requireAuth);
 
 router.get("/", async (request, response) => {
-    try{
-    //grab the _id to identity which the user's friendlist
-    const userId = request.user._id; // Access _id directly from the user object
+    try {
+        //Grab the _id to identify the user's friend list
+        //turn _id from string to mongoDB ObjectId class
+        const userId = new ObjectId(request.user._id); 
+        console.log("User ID:", userId);
 
-    //query mongoDb
-    //const friendData = await Users.collection.find({ _id: userId }).project({ friends: 1 }).toArray();
-    const friendData = await Users.collection.find({ _id: userId }).toArray();
-    const jsonData = { friendData };
-    return response.status(200).json(jsonData);
-    //send response
-    // return response.status(200).json(friendData);
+        //Query MongoDB to find the user's document
+        const user = await Users.collection.findOne({ _id: userId });
+        if (!user) {
+            console.log("User not found in the database.");
+            return response.status(404).json({ error: "User not found" });
+        }
 
-    }
-    catch(error){
-        console.log(error);
-        return response.status(500).send({message: error.message})
+        //Retrieve the friend data from the user document
+        const friendDataArray = user.friends;
+        console.log("Friend Data:", friendDataArray);
+
+        //set to json and return 
+        const jsonData = { friendDataArray };
+        console.log(jsonData);
+        return response.status(200).json(jsonData);
+
+    } catch (error) {
+        console.log("Error:", error);
+        return response.status(500).send({ message: error.message });
     }
 });
 
