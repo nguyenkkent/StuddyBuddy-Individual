61ba2ce1c69cb963669b2f8e1a77731aea880c73
yyyuquan <yxu15@mail.sfsu.edu>
2024-04-24 14:46:18 -0700

    Added create-group page for users to create new groups based on (not working) friendlist

---
+++ application/frontend/src/components/pages/create-group.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const CreateNewGroup = () => {
+    const [groupName, setGroupName] = useState('');
+    const [friends, setFriends] = useState([]); // Change this to the actual list of friends
+    const [selectedFriends, setSelectedFriends] = useState(new Set());
+    const navigate = useNavigate();
+
+    const handleGroupNameChange = (event) => {
+        setGroupName(event.target.value);
+    };
+
+    const handleSearchFriends = (searchTerm) => {
+        // REPLACE WITH THE ACTUAL API CALL - I don't want to put in something that isn't set up -yq
+    };
+
+    const handleSelectFriend = (friendId) => {
+        setSelectedFriends(prevSelectedFriends => {
+        const newSelectedFriends = new Set(prevSelectedFriends);
+        if (newSelectedFriends.has(friendId)) {
+        newSelectedFriends.delete(friendId);
+        } else {
+            newSelectedFriends.add(friendId);
+        }
+        return newSelectedFriends;
+        });
+    };
+
+    const handleCreateGroup = () => {
+        const group = {
+        name: groupName,
+        members: Array.from(selectedFriends),
+        };
+        // REPLACE WITH THE ACTUAL API CALL - I don't want to put in something that isn't set up -yq
+        console.log('Creating group with:', group);
+        // Not sure if you want the form to reset or redirect the user
+    };
+
+  return (
+    <div>
+      <input type="text" value={groupName} onChange={handleGroupNameChange} placeholder="Group Name" />
+      {friends.map(friend => (
+        <div
+          key={friend._id}
+          className={`friend-item ${selectedFriends.has(friend._id) ? 'selected' : ''}`}
+          onClick={() => handleSelectFriend(friend._id)}
+        >
+          {friend.name} {/* Some cool styles can be added here */}
+        </div>
+      ))}
+      <button onClick={handleCreateGroup}>Add</button>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </div>
+  );
+};
+
+export default CreateNewGroup;
