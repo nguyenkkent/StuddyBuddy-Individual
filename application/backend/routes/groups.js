2c23a730994e980c038e78299074837f4bf352ab
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-03 14:51:55 -0700

    Added first query check to see if user exists in database

--- application/backend/routes/groups.js
+++ application/backend/routes/groups.js
@@ -7,5 +7,26 @@ import {ObjectId} from "mongodb";
 const router = express.Router();
 router.use(requireAuth);
 
+router.get("/", async (request, response) => {
+    try{
+        //Grab the _id to identify the user's friend list
+        //turn _id from string to mongoDB ObjectId class
+        const userId = new ObjectId(request.user._id); 
+        console.log("User ID:", userId);    
+
+        //Query MongoDB to find the user's document
+        const user = await Groups.collection.findOne({ _id: userId });
+        if (!user) {
+            console.log("User not found in the database.");
+            return response.status(404).json({ error: "User not found" });
+        }
+        //assertion : user exists in the database
+
+    }catch(error){
+        console.log("Error:", error);
+        return response.status(500).send({ message: error.message });        
+    }
+});
+
 
 export default router;
