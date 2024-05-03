73fff72b977ee98f5e157dd5fc04f01fb3f797b5
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-03 14:54:24 -0700

    Changed message schema to differentiate between message betweeo two individuals or a group message

--- application/backend/models/messageSchema.js
+++ application/backend/models/messageSchema.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema;
 
 const messageSchema = new mongoose.Schema({
     participants: [String],
-    contents: [String]
+    contents: [String],
+    isGroupMessage: {
+        type: Boolean,
+        require: true,
+    }
+
 })
  
 export const Messages = mongoose.model("Messages", messageSchema);
