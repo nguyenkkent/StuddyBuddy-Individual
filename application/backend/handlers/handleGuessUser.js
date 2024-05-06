116e81c125eec1e723ff4513c8266349b280ef0f
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-06 11:02:34 -0700

    Separated guess user registration into it's own handler function

---
+++ application/backend/handlers/handleGuessUser.js
@@ -0,0 +1,38 @@
+import { Users } from "../models/userSchema";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'});
+}
+
+export async function handleGuessUser(request, response) {
+    try{
+        const newUser = {
+            username : request.body.username,
+            tags: [],
+            history:[],
+            isVerified: false,
+            isGuess: true
+            }
+        const result = await Users.collection.insertOne(newUser);
+        //append the last 6 character of document's _id to guest username
+        const objectId = result.insertedId;  
+        const suffix = objectId.toString().slice(-6);
+        newUser.username += suffix;
+
+        // Update the user in the database with the new username
+        await Users.collection.updateOne({ _id: objectId }, { $set: { username: newUser.username } });
+
+        //create variables for json return
+        const username = newUser.username;
+        const token = createToken(result.insertedId);
+
+        console.log(`A guest was inserted with the _id: ${result.insertedId}`);
+        return response.status(200).json({username, token});
+    }
+catch(error){
+    console.log("Error: ", error.message);
+    response.status(500).send({message: error.message})
+}
+};
--- application/backend/routes/register.js
+++ application/backend/routes/register.js
@@ -2,50 +2,12 @@ import express from "express";
 //import {Users} from "../models/userSchema.js";
 // import "dotenv/config";
 import { handleRegistration } from "../handlers/handleRegisterUser";
+import { handleGuessUser } from "../handlers/handleGuessUser";
 
 const router = express.Router();
 
 router.post('/', handleRegistration);
 
-
-router.post('/guest', async (request, response)=>{
-    try{
-            const newUser = {
-                username : request.body.username,
-                tags: [],
-                history:[],
-                isVerified: false,
-                isGuess: true
-                }
-            const result = await Users.collection.insertOne(newUser);
-            //append the last 6 character of document's _id to guest username
-            const objectId = result.insertedId;  
-            const suffix = objectId.toString().slice(-6);
-            newUser.username += suffix;
-
-            // Update the user in the database with the new username
-            await Users.collection.updateOne({ _id: objectId }, { $set: { username: newUser.username } });
-
-            //create variables for json return
-            const username = newUser.username;
-            const token = createToken(result.insertedId);
-
-            console.log(`A guest was inserted with the _id: ${result.insertedId}`);
-            return response.status(200).json({username, token});
-        }
-    catch(error){
-        console.log("Error: ", error.message);
-        response.status(500).send({message: error.message})
-    }
-})
-
-// router.post("/", async (request, response) => {
-//     try {
-//         return response.status(200);
-//     } catch (error) {
-//         console.error("Error:", error.message);
-//         return response.status(500);
-//     }
-// });
+router.post('/guest', handleGuessUser)
 
 export default router;
