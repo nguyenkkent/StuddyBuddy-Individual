751f2ec0ce7fcc7d337ab04c1d17f4faebab2a69
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-06 19:08:16 -0700

    Fixed issue with imports for Users schema

--- application/backend/routes/register.js
+++ application/backend/routes/register.js
@@ -1,13 +1,13 @@
 import express from "express";
 //import {Users} from "../models/userSchema.js";
 // import "dotenv/config";
-import { handleRegistration } from "../handlers/handleRegisterUser";
-import { handleGuessUser } from "../handlers/handleGuessUser";
+import { handleRegistration } from "../handlers/handleRegisterUser.js";
+import { handleGuessUser } from "../handlers/handleGuessUser.js";
 
 const router = express.Router();
 
-router.post('/', handleRegistration);
+// router.post('/', handleRegistration);
 
-router.post('/guest', handleGuessUser)
+// router.post('/guest', handleGuessUser)
 
 export default router;
