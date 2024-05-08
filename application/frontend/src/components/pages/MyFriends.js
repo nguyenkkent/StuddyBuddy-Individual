88ba675a4296eaa3212259117b8559d7a3a289fa
Ashley Ching <aching4@sfsu.edu>
2024-05-08 00:00:30 -0700

    Add Friend + Create Group + My Groups styling

--- application/frontend/src/components/pages/MyFriends.js
+++ application/frontend/src/components/pages/MyFriends.js
@@ -79,7 +79,7 @@ function MyFriends() {
           <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="dashboard-search-button">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
-          <button><Link to="/add-friend">Add friend</Link></button>
+          <Link className="add-create-button" to="/add-friend">Add Friend</Link>
         </div>
         <Select
           className="dashboard-filter"
--- application/frontend/src/components/pages/MyGroups.js
+++ application/frontend/src/components/pages/MyGroups.js
@@ -47,6 +47,7 @@ const MyGroups = () => {
           <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="dashboard-search-button">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
+          <Link className="add-create-button" to="/create-group">Create Group</Link>
         </div>
         <div className="group-results">
           {filteredGroups && filteredGroups.map(group => (
