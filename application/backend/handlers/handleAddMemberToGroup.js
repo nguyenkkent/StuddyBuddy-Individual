c77e457b4a55680c5e06224afe020bec69d94eab
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-10 23:17:31 -0700

    Added basic files to add member to a group

---
+++ application/backend/handlers/handleAddMemberToGroup.js
@@ -0,0 +1,16 @@
+import { ObjectId } from "mongodb";
+import { Groups } from "../models/groupSchema.js";
+import { Users } from "../models/userSchema.js";
+
+
+export async function handleAddMemberToGroup(request, response){
+
+    
+    //locate the group document in database
+
+    //find new member's document in database
+
+    //add new member into the group
+
+
+};
