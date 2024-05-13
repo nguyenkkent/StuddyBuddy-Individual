cbf80167ebb2fe543be3e7e6cf840e157d5ef1f2
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-13 10:59:02 -0700

    Created send-message route

---
+++ application/backend/handlers/chats/handleSendMessage.js
@@ -0,0 +1,10 @@
+import { Users } from "../../models/userSchema.js";
+import { Messages } from "../../models/messageSchema.js";
+
+export async function handleSendMessage(request, response){
+    try{
+
+    }catch(error){
+        
+    }
+}
--- application/backend/routes/chats.js
+++ application/backend/routes/chats.js
@@ -1,10 +1,12 @@
 import express from "express";
 import requireAuth from "../middleware/requireAuth.js"
 import { handleStartChats } from "../handlers/chats/handleStartChat.js"
+import { handleSendMessage } from "../handlers/chats/handleSendMessage.js";
 
 const router = express.Router();
 router.use(requireAuth);
 
 router.post("/start-chat", handleStartChats);
+router.post("/send-message/", handleSendMessage);
 
 export default router;
