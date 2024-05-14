2458cf35221b220ea4d4faab5cad4dc83f6c5a06
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-14 13:34:39 -0700

    Connected handleSelectFriend function to the backend

--- application/frontend/src/components/pages/CreateNewGroup.js
+++ application/frontend/src/components/pages/CreateNewGroup.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axiosClient from "../../axiosClient";
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const CreateNewGroup = () => {
+    const { user } = useAuthContext(); 
     const [groupName, setGroupName] = useState('');
     const [friends, setFriends] = useState([]); // Change this to the actual list of friends
     const [selectedFriends, setSelectedFriends] = useState(new Set());
     const navigate = useNavigate();
 
-    const handleGroupNameChange = (event) => {
+    const handleGroupNameChange = async (event) => {
         setGroupName(event.target.value);
     };
-
-    const handleSearchFriends = (searchTerm) => {
-        // REPLACE WITH THE ACTUAL API CALL - I don't want to put in something that isn't set up -yq
-        // Should return the friendlist that match the search term
+    
+    const handleSearchFriends = async (searchTerm) => {
+        try{
+          const response = await axiosClient.get("/api/my-groups/search-friend-list", 
+            {
+              searchTerm
+            },
+            {
+              headers: {
+                'Authorization': `Bearer ${user.token}`
+              }      
+            });
+        }catch(error){
+          console.log("handleSearchFriends error: ", error);
+        }
     };
 
     const handleSelectFriend = (friendId) => {
