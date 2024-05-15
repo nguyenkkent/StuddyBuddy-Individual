b95579ed8b3c19a7ee4e32ae09d0cad5b72e9986
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-15 10:35:53 -0700

    Migrated add-friend to friend route

--- application/backend/routes/friends.js
+++ application/backend/routes/friends.js
@@ -1,11 +1,15 @@
 import express from "express";
 import requireAuth from "../middleware/requireAuth.js"
 import { handleDisplayAllFriends } from "../handlers/friends/handleDisplayAllFriends.js";
+import { handleSearchForUsers } from "../handlers/friends/handleSearchForUser.js";
+import { handleAddFriend } from "../handlers/friends/handleAddFriend.js";
 
 
 const router = express.Router();
 router.use(requireAuth);
 
 router.get("/", handleDisplayAllFriends);
+router.get("/search-for-users", handleSearchForUsers);
+router.post("/add-friend", handleAddFriend);
 
 export default router;
